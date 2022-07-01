d45818e1f70351174a914e7e37806882b7c85033
G00398318@gmit.ie <G00398318@gmit.ie>
2022-07-01 11:14:12 +0100

    Add Banner and Kivy icon

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ https://buildozer.readthedocs.io/en/latest/installation.html
 https://www.youtube.com/watch?v=ah3JeHAfM0M
 
 https://github.com/MariyaSha/BinarytoDecimal
+
+## Kivy Icon list
+https://kivymd.readthedocs.io/en/latest/themes/icon-definitions/index.html
+
+### Create an Kivy app to look up icons
+
+https://kivymd.readthedocs.io/en/latest/themes/icon-definitions/index.html#to-preview-the-icons-and-their-names-you-can-use-the-following-application
+
+
---
+++ create_app_02.py
@@ -0,0 +1,35 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.app import MDApp
+from kivy.uix.image import Image
+from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.label import MDLabel
+from kivymd.uix.toolbar import MDToolbar
+from matplotlib.pyplot import title
+
+class ConverterApp(MDApp):
+
+    def flip(self):
+        print("Hello World")
+
+
+    def build(self):
+        screen = MDScreen()
+        
+
+        # Add top banner
+        self.toolbar = MDToolbar(title="Binary to Decimal")
+        self.toolbar.pos_hint = {"top": 1}
+        # Add icon to banner and add a function def flip(self)
+        # https://kivymd.readthedocs.io/en/latest/themes/icon-definitions/index.html
+        self.toolbar.right_action_items = [
+            ["rotate-3d", lambda x: self.flip()]]
+            
+
+        screen.add_widget(self.toolbar)
+
+
+        return screen
+
+if __name__ == '__main__':
+    ConverterApp().run()
